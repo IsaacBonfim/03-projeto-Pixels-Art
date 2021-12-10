b872c602c13b24a70fb40aec5fafced04187be29
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2021-12-10 17:22:19 -0300

    Realizaçao de alguns ajustes

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 const board = document.getElementById('pixel-board');
+const valorBase = document.querySelector('#base');
 
-for (let i = 0; i < 5; i += 1) {
-  const line = document.createElement('div');
+function ampliaTabela(evento) {
+  localStorage.setItem('valorBase.value', evento.target.value);
+}
 
-  line.style.height = `${40}px`;
+const valorStorage = localStorage.getItem('valorBase.value');
 
-  for (let j = 0; j < 5; j += 1) {
-    const square = document.createElement('div');
-
-    square.className = 'pixel';
-
-    line.appendChild(square);
+function inicializaBase() {
+  if (valorStorage !== null) {
+    valorBase.value = valorStorage;
   }
 
-  board.appendChild(line);
+  return valorBase.value;
 }
+
+function criaTabela() {
+  const base = inicializaBase();
+
+  for (let i = 0; i < base; i += 1) {
+    const line = document.createElement('div');
+
+    line.style.height = `${40}px`;
+
+    for (let j = 0; j < base; j += 1) {
+      const square = document.createElement('div');
+
+      square.className = 'pixel';
+
+      line.appendChild(square);
+    }
+
+    board.appendChild(line);
+  }
+  return null;
+}
+
+window.onload = criaTabela();
+
+valorBase.addEventListener('click', ampliaTabela);
+valorBase.addEventListener('keyup', ampliaTabela);
