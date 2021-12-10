48d00a60c2c915be39e9b7bb8b55002a982077cd
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2021-12-09 22:07:28 -0300

    Envio do requisito 5

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const board = document.getElementById('pixel-board');
 for (let i = 0; i < 5; i += 1) {
   const line = document.createElement('div');
 
-  line.style.height = `${80}px`;
+  line.style.height = `${40}px`;
 
   for (let j = 0; j < 5; j += 1) {
     const square = document.createElement('div');
