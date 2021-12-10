c51892879c6ecf01ff9b8afab4e487b076a26802
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2021-12-09 21:54:38 -0300

    Envio do requisito 4

---
+++ script.js
@@ -0,0 +1,17 @@
+const board = document.getElementById('pixel-board');
+
+for (let i = 0; i < 5; i += 1) {
+  const line = document.createElement('div');
+
+  line.style.height = `${80}px`;
+
+  for (let j = 0; j < 5; j += 1) {
+    const square = document.createElement('div');
+
+    square.className = 'pixel';
+
+    line.appendChild(square);
+  }
+
+  board.appendChild(line);
+}
